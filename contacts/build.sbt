cd9dddba80492ad99deefe2e05b9e771fe695b8d
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2020-02-13 00:22:00 +0100

    (contacts): Init project

---
+++ contacts/build.sbt
@@ -0,0 +1,38 @@
+import Dependencies._
+
+lazy val info = List(
+  name := "contacts",
+  version := "1.0.0",
+  organization := "com.github.drmarjanovic",
+  description := "Service used for managing contacts."
+)
+
+lazy val protocol =
+  project
+    .in(file("modules/protocol"))
+    .settings(
+      version := "1.0.0",
+      organization := "com.github.drmarjanovic.contacts",
+      name := "protocol"
+    )
+    .settings {
+      scalaVersion := "2.13.1"
+      libraryDependencies ++= List(api, circe)
+    }
+
+lazy val root = (project in file("."))
+  .settings(info: _*)
+  .settings {
+    scalaVersion := "2.13.1"
+    libraryDependencies ++= {
+      val commons = List(tracing)
+      val core    = List(cats, http4sBlazeServer, http4sCirce, http4sDsl, pureConfig)
+      val db      = List(doobieCore, doobieHikari, doobiePostgres, flyway)
+      val utils   = List(jodaTime)
+      val zio     = List(zioCore, zioInteropCats)
+      val test    = List(zioTest).map(_ % Test)
+
+      commons ++ core ++ db ++ utils ++ zio ++ test
+    }
+  }
+  .dependsOn(protocol)
---
+++ contacts/project/Dependencies.scala
@@ -0,0 +1,41 @@
+import sbt._
+
+object Dependencies {
+
+  private object Commons {
+    val Api     = "1.0.1"
+    val Tracing = "1.0.0"
+  }
+
+  private object Versions {
+    val Cats           = "2.1.0"
+    val Circe          = "0.12.3"
+    val Doobie         = "0.8.8"
+    val Flyway         = "6.1.1"
+    val Http4s         = "0.21.0-M6"
+    val JodaTime       = "2.10.5"
+    val PureConfig     = "0.12.2"
+    val Zio            = "1.0.0-RC17"
+    val ZioInteropCats = "2.0.0.0-RC10"
+  }
+
+  val api: ModuleID     = "com.github.drmarjanovic.commons" %% "api"     % Commons.Api
+  val tracing: ModuleID = "com.github.drmarjanovic.commons" %% "tracing" % Commons.Tracing
+
+  val cats: ModuleID              = "org.typelevel"         %% "cats-core"           % Versions.Cats
+  val circe: ModuleID             = "io.circe"              %% "circe-generic"       % Versions.Circe
+  val doobieCore: ModuleID        = "org.tpolecat"          %% "doobie-core"         % Versions.Doobie
+  val doobieHikari: ModuleID      = "org.tpolecat"          %% "doobie-hikari"       % Versions.Doobie
+  val doobiePostgres: ModuleID    = "org.tpolecat"          %% "doobie-postgres"     % Versions.Doobie
+  val flyway: ModuleID            = "org.flywaydb"          % "flyway-core"          % Versions.Flyway
+  val http4sCore: ModuleID        = "org.http4s"            %% "http4s-core"         % Versions.Http4s
+  val http4sBlazeServer: ModuleID = "org.http4s"            %% "http4s-blaze-server" % Versions.Http4s
+  val http4sCirce: ModuleID       = "org.http4s"            %% "http4s-circe"        % Versions.Http4s
+  val http4sDsl: ModuleID         = "org.http4s"            %% "http4s-dsl"          % Versions.Http4s
+  val jodaTime: ModuleID          = "joda-time"             % "joda-time"            % Versions.JodaTime
+  val pureConfig: ModuleID        = "com.github.pureconfig" %% "pureconfig"          % Versions.PureConfig
+  val zioCore: ModuleID           = "dev.zio"               %% "zio"                 % Versions.Zio
+  val zioInteropCats: ModuleID    = "dev.zio"               %% "zio-interop-cats"    % Versions.ZioInteropCats
+  val zioTest: ModuleID           = "dev.zio"               %% "zio-test"            % Versions.Zio
+
+}
